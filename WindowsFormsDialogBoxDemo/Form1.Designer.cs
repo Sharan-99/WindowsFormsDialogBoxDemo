8ec453a0b122ddc87e9895952b9ecf86765e6c7f
Sharan-99 <sharan.ceg2017103591@gmail.com>
2022-05-25 12:57:13 +0530

    first commit

---
+++ WindowsFormsDialogBoxDemo/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace WindowsFormsDialogBoxDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFont = new System.Windows.Forms.Button();
+            this.lblFont = new System.Windows.Forms.Label();
+            this.btnColor = new System.Windows.Forms.Button();
+            this.btnChooseFile = new System.Windows.Forms.Button();
+            this.btnSaveLocation = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnFont
+            // 
+            this.btnFont.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFont.Location = new System.Drawing.Point(12, 54);
+            this.btnFont.Name = "btnFont";
+            this.btnFont.Size = new System.Drawing.Size(133, 33);
+            this.btnFont.TabIndex = 0;
+            this.btnFont.Text = "Choose Fonts";
+            this.btnFont.UseVisualStyleBackColor = true;
+            this.btnFont.Click += new System.EventHandler(this.btnFont_Click);
+            // 
+            // lblFont
+            // 
+            this.lblFont.AutoSize = true;
+            this.lblFont.Location = new System.Drawing.Point(97, 135);
+            this.lblFont.Name = "lblFont";
+            this.lblFont.Size = new System.Drawing.Size(109, 13);
+            this.lblFont.TabIndex = 1;
+            this.lblFont.Text = "Hello Windows Forms";
+            this.lblFont.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnColor
+            // 
+            this.btnColor.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnColor.Location = new System.Drawing.Point(178, 54);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(134, 33);
+            this.btnColor.TabIndex = 2;
+            this.btnColor.Text = "Choose Color";
+            this.btnColor.UseVisualStyleBackColor = true;
+            this.btnColor.Click += new System.EventHandler(this.btnColor_Click);
+            // 
+            // btnChooseFile
+            // 
+            this.btnChooseFile.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChooseFile.Location = new System.Drawing.Point(394, 55);
+            this.btnChooseFile.Name = "btnChooseFile";
+            this.btnChooseFile.Size = new System.Drawing.Size(116, 33);
+            this.btnChooseFile.TabIndex = 3;
+            this.btnChooseFile.Text = "Choose File";
+            this.btnChooseFile.UseVisualStyleBackColor = true;
+            this.btnChooseFile.Click += new System.EventHandler(this.btnChooseFile_Click);
+            // 
+            // btnSaveLocation
+            // 
+            this.btnSaveLocation.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSaveLocation.Location = new System.Drawing.Point(581, 54);
+            this.btnSaveLocation.Name = "btnSaveLocation";
+            this.btnSaveLocation.Size = new System.Drawing.Size(161, 34);
+            this.btnSaveLocation.TabIndex = 4;
+            this.btnSaveLocation.Text = "Choose Save Location";
+            this.btnSaveLocation.UseVisualStyleBackColor = true;
+            this.btnSaveLocation.Click += new System.EventHandler(this.btnSaveLocation_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSaveLocation);
+            this.Controls.Add(this.btnChooseFile);
+            this.Controls.Add(this.btnColor);
+            this.Controls.Add(this.lblFont);
+            this.Controls.Add(this.btnFont);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFont;
+        private System.Windows.Forms.Label lblFont;
+        private System.Windows.Forms.Button btnColor;
+        private System.Windows.Forms.Button btnChooseFile;
+        private System.Windows.Forms.Button btnSaveLocation;
+    }
+}
+
---
+++ WindowsFormsDialogBoxDemo/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsDialogBoxDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnFont_Click(object sender, EventArgs e)
+        {
+            var fonts = new FontDialog();
+            var result = fonts.ShowDialog();
+            if(result==DialogResult.OK)
+            {
+                lblFont.Font = fonts.Font;
+            }
+        }
+
+        private void btnColor_Click(object sender, EventArgs e)
+        {
+            var color = new ColorDialog();
+            var result = color.ShowDialog();
+            if(result==DialogResult.OK)
+            {
+                lblFont.ForeColor = color.Color;
+            }
+        }
+
+        private void btnChooseFile_Click(object sender, EventArgs e)
+        {
+            var open = new OpenFileDialog();
+            var result = open.ShowDialog();
+            if(result==DialogResult.OK)
+            {
+                var filepath = open.FileName;
+                MessageBox.Show(filepath);
+            }
+        }
+
+        private void btnSaveLocation_Click(object sender, EventArgs e)
+        {
+            var savefiledialog = new SaveFileDialog();
+            var result = savefiledialog.ShowDialog();
+            if(result==DialogResult.OK)
+            {
+                var filepath = savefiledialog.FileName;
+
+                using(var fs=new FileStream(filepath,FileMode.CreateNew,FileAccess.Write))
+                {
+                    using(var sw=new StreamWriter(fs))
+                    {
+                        sw.WriteLine("Hello Sharan");
+                        sw.Close();
+                    }
+                    fs.Close();
+                }
+                MessageBox.Show(filepath);
+            }
+        }
+    }
+}
